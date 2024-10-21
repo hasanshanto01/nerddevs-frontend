1fb22719b43f3776dafaa671a21e615299807cd9
hasanshanto01 <hasanshanto01@gmail.com>
2024-10-22 02:11:48 +0600

    basic app setup

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "primary-green": "#4caf50",
+        "primary-dark-green": "#388e3c",
+        "common-white": "#ffffff",
+        "common-black": "#000000",
+        "primary-gradient": "linear-gradient(135deg, #f9f9f9 0%, #e0f7fa 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
