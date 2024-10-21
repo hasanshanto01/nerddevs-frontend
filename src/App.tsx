522e9918de24727309f5d080544d8eff4ffd5a04
hasanshanto01 <hasanshanto01@gmail.com>
2024-10-22 02:20:15 +0600

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/Home/Home";
+import Registration from "./pages/Auth/Registration/Registration";
+import Login from "./pages/Auth/Login/Login";
+
 function App() {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="/" element={<Home />} />
+          </Route>
+
+          {/* auth routes */}
+          <Route path="/registration" element={<Registration />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
